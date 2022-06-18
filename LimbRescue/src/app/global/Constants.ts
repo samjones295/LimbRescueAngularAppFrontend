2abc2957c379df40828b64a0fc21c3eef410a630
patton450 <patton.450@osu.edu>
2022-06-18 17:16:08 -0400

    https://localhost as the path

--- LimbRescue/src/app/global/Constants.ts
+++ LimbRescue/src/app/global/Constants.ts
@@ -1,6 +1,7 @@
 export class Constants {
     // PROD
     //public static IP: string = 'https://backend.limbrescue.com'
-    // TESTING
-    public static IP: string= "http://localhost:8081/api"
+    
+    //public static IP: string = 'http://localhost:8081/api'
+    public static IP: string = 'https://localhost/api'
 }
