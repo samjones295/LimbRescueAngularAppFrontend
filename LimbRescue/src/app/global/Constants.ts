f5ad0b71bd217644728feb8d2901d7160d1a015c
EC2 Default User <ec2-user@ip-172-31-95-247.ec2.internal>
2023-02-23 02:02:35 +0000

    test

--- LimbRescue/src/app/global/Constants.ts
+++ LimbRescue/src/app/global/Constants.ts
@@ -1,6 +1,6 @@
 export class Constants {
     // PROD
-    public static IP: string = 'https://limbrescue.com/api'
+    public static IP: string = '44.203.124.100';
     
     //public static IP: string = 'http://localhost:8081/api'
     //public static IP: string = 'https://localhost/api'
